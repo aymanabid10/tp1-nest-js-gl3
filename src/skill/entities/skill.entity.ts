2dd6f0fc244031c9579d57b94a2afc20c80c4dae
elyes-ST <elyes.mlawah@singleton.tn>
2026-04-01 22:31:41 +0100

    feat: define User, Skill, Cv entities with relations

--- src/skill/entities/skill.entity.ts
+++ src/skill/entities/skill.entity.ts
@@ -1,1 +1,14 @@
-export class Skill {}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany } from 'typeorm';
+import { Cv } from '../../cv/entities/cv.entity';
+
+@Entity()
+export class Skill {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  designation!: string;
+
+  @ManyToMany(() => Cv, (cv) => cv.skills)
+  cvs!: Cv[];
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Cv } from '../../cv/entities/cv.entity';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  username!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @OneToMany(() => Cv, (cv) => cv.user)
+  cvs!: Cv[];
+}
