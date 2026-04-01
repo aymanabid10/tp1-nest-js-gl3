c6499e308e3d1262b818a2725b7f499e9c4d13e1
elyes-ST <elyes.mlawah@singleton.tn>
2026-04-01 22:26:30 +0100

    feat: configure TypeORM connection in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { CvModule } from './cv/cv.module';
 import { UserModule } from './user/user.module';
 import { SkillModule } from './skill/skill.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CvModule, UserModule, SkillModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'cv_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // create the tables auto : en dev seulement
+    }),
+    CvModule,
+    UserModule,
+    SkillModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
