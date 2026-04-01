5063eb50dc5075bbf105b537b1bb9db2301f7036
Aymen Abid <aymen.abid2023@gmail.com>
2026-04-01 21:16:13 +0100

    Docs (Swagger) : Create swagger setup for the project

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,19 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Tp1 NestJS API GL3')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    //.addBearerAuth() // TODO : Add JWT authentication if needed later
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable Swagger only in development mode
+  // route to access Swagger UI: /api/docs
+  if (process.env.NODE_ENV !== 'production') {
+    setupSwagger(app);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
