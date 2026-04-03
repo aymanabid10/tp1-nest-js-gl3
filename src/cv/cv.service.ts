d55e7b76fea1a3e67033fc52dbb78ab42aff1e25
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2026-04-03 19:31:06 +0100

    Update CvService to handle userId from auth and implement ownership checks

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Cv } from './entities/cv.entity';
 import { CreateCvDto } from './dto/create-cv.dto';
 import { GenericService } from '../common/services/generic.service';
+import { UpdateCvDto } from './dto/update-cv.dto';
 
 @Injectable()
 export class CvService extends GenericService<Cv> {
@@ -14,12 +15,40 @@ export class CvService extends GenericService<Cv> {
     super(cvRepository);
   }
 
-  async create(createCvDto: CreateCvDto): Promise<Cv> {
+  async create(createCvDto: CreateCvDto & { userId: number }): Promise<Cv> {
     const cv = this.cvRepository.create({
       ...createCvDto,
       user: { id: createCvDto.userId },
       skills: createCvDto.skillIds?.map((id) => ({ id })) ?? [],
     });
     return this.cvRepository.save(cv);
+  }
+
+  async findOne(id: number): Promise<Cv> {
+    const cv = await this.cvRepository.findOne({ where: { id } });
+
+    if (!cv) {
+      throw new NotFoundException(`Cv with id ${id} not found`);
+    }
+
+    return cv;
+  }
+
+  async update(id: number, updateCvDto: UpdateCvDto): Promise<Cv> {
+    const cv = await this.findOne(id);
+    const { skillIds, ...cvData } = updateCvDto;
+
+    Object.assign(cv, cvData);
+
+    if (skillIds !== undefined) {
+      cv.skills = skillIds.map((skillId) => ({ id: skillId })) as Cv['skills'];
+    }
+
+    return this.cvRepository.save(cv);
+  }
+
+  async remove(id: number): Promise<void> {
+    const cv = await this.findOne(id);
+    await this.cvRepository.remove(cv);
   }
 }
