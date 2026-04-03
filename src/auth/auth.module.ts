6415a13714c2b27f35457d736b56786d10bece03
Aymen Abid <aymen.abid2023@gmail.com>
2026-04-03 22:40:37 +0100

    feat : create a mocking email sender for auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,20 +3,44 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/passport-jwt.strategy';
+import { ValidateUserHandler } from './handlers/signin/validate-user.handler';
+import { CheckPasswordHandler } from './handlers/signin/check-password.handler';
+import { GenerateJwtHandler } from './handlers/signin/generate-jwt.handler';
+import { PassportModule } from '@nestjs/passport';
+import { CheckEmailHandler } from './handlers/signup/check-email.handler';
+import { ValidateSignupHandler } from './handlers/signup/validate.handler';
+import { SaveUserHandler } from './handlers/signup/save-user.handler';
+import { HashPasswordHandler } from './handlers/signup/hash-password.handler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports :[
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'default_secret',
       signOptions: {
         expiresIn: '1d',
       },
-    })
+    }),
+    TypeOrmModule.forFeature([User]), // TODO : remove this and use UserModule instead when is fully implemented
+    UserModule
   ],
   controllers: [AuthController],
   providers: [
     AuthService,
-    JwtStrategy
+    JwtStrategy,
+    JwtModule,
+
+    //CoR Handlers
+    ValidateSignupHandler,
+    ValidateUserHandler,
+    SaveUserHandler,
+    HashPasswordHandler,
+    CheckPasswordHandler,
+    CheckEmailHandler,
+    GenerateJwtHandler,
   ]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { HashPasswordHandler } from './handlers/signup/hash-password.handler';
+import { SaveUserHandler } from './handlers/signup/save-user.handler';
+import { ValidateSignupHandler } from './handlers/signup/validate.handler';
+import { CheckEmailHandler } from './handlers/signup/check-email.handler';
+import { SignupDto } from './dto/sign-up.dto';
+import { CheckPasswordHandler } from './handlers/signin/check-password.handler';
+import { GenerateJwtHandler } from './handlers/signin/generate-jwt.handler';
+import { SigninDto } from './dto/sign-in.dto';
+import { ValidateUserHandler } from './handlers/signin/validate-user.handler';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private validate: ValidateSignupHandler,
+    private checkEmail : CheckEmailHandler,
+    private hash: HashPasswordHandler,
+    private save: SaveUserHandler,
+    private validateUserHandler : ValidateUserHandler,
+    private checkPasswordHandler : CheckPasswordHandler,
+    private jwtHandler: GenerateJwtHandler
+    //private email: SendEmailHandler,
+  ) {}
+
+  async signup(dto: SignupDto) {
+    this.validate
+      .setNext(this.checkEmail)
+      .setNext(this.hash)
+      .setNext(this.save)
+      //.setNext(this.email)
+
+    return this.validate.handle(dto);
+  }
+
+  async signin(dto: SigninDto) {
+    this.validateUserHandler
+      .setNext(this.checkPasswordHandler)
+      .setNext(this.jwtHandler);
+
+    return this.validateUserHandler.handle(dto);
+  }
+
+}
