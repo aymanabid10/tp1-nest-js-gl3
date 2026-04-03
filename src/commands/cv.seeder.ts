3150abc2e6384d30d854fac90d0f78838c6db688
Aymen Abid <aymen.abid2023@gmail.com>
2026-04-03 23:41:26 +0100

    fix (seeder) : hashing password + add salt to all seeders

--- src/commands/cv.seeder.ts
+++ src/commands/cv.seeder.ts
@@ -16,6 +16,8 @@ import {
   randSkill,
 } from '@ngneat/falso';
 import { Role } from 'src/shared/enums/role.enum';
+import { HashPasswordHandler } from 'src/auth/handlers/signup/hash-password.handler';
+import { SignupDto } from 'src/auth/dto/sign-up.dto';
 
 async function bootstrap() {
   const logger = new Logger('Seeder');
@@ -24,6 +26,7 @@ async function bootstrap() {
   const userService = app.get(UserService);
   const skillService = app.get(SkillService);
   const cvService = app.get(CvService);
+  const PasswordHash = app.get(HashPasswordHandler);
 
   logger.log('Démarrage du seeding\n');
 
@@ -41,12 +44,14 @@ async function bootstrap() {
   //2. Seed Users
   const users: User[] = [];
   for (let i = 0; i < 3; i++) {
-    const user = await userService.create({
+    let dto = {
       username: randUserName(),
       role: Role.USER,
       email: randEmail(),
       password: 'password123',
-    });
+    } as SignupDto;
+    dto = await PasswordHash.handle(dto) as SignupDto;
+    const user = await userService.create(dto);
     users.push(user);
     logger.log(`User créé : ${user.username} (${user.email})`);
   }
