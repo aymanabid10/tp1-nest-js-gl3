c18eed51e77c7ad9fa326d58778ae5e8ffc125d1
Aymen Abid <aymen.abid2023@gmail.com>
2026-04-03 20:04:24 +0100

    feat : Create passport jwt strat

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/passport-jwt.strategy';
 
 @Module({
+  imports :[
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default_secret',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [
+    AuthService,
+    JwtStrategy
+  ]
 })
 export class AuthModule {}
