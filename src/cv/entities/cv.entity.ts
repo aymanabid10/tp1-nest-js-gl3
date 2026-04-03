65ae6158ed388e08e20e3605dda2be41c2b43cb7
YassWorks <mohamedyassine.kallel@insat.ucar.tn>
2026-04-03 19:35:03 +0100

    removed french comment (i hate french)

--- src/cv/entities/cv.entity.ts
+++ src/cv/entities/cv.entity.ts
@@ -36,6 +36,6 @@ export class Cv {
   user!: User;
 
   @ManyToMany(() => Skill, (skill) => skill.cvs, { eager: true })
-  @JoinTable() // JoinTable uniquement du côté propriétaire (Cv)
+  @JoinTable()
   skills!: Skill[];
 }
