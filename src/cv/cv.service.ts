514ce89ecc8f11abfa367d5c466bd65f736b183a
MakkiAloulou <makkialoulou2005@gmail.com>
2026-05-01 00:00:20 +0100

    feat(cv): emit cv history audit events from service

--- src/cv/cv.service.ts
+++ src/cv/cv.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { EventEmitter2 } from '@nestjs/event-emitter';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { PayloadInterface } from 'src/auth/interface/payload.interface';
@@ -9,36 +10,67 @@ import { Cv } from './entities/cv.entity';
 import { CreateCvDto } from './dto/create-cv.dto';
 import { UpdateCvDto } from './dto/update-cv.dto';
 import { GenericService } from '../common/services/generic.service';
+import { CvCreateStartedEvent } from 'src/cv-history/domain/events/cv-create-started.event';
+import { CvCreatedEvent } from 'src/cv-history/domain/events/cv-created.event';
+import { CvReadEvent } from 'src/cv-history/domain/events/cv-read.event';
+import { CvUpdateStartedEvent } from 'src/cv-history/domain/events/cv-update-started.event';
+import { CvUpdatedEvent } from 'src/cv-history/domain/events/cv-updated.event';
 
 @Injectable()
 export class CvService extends GenericService<Cv> {
   constructor(
     @InjectRepository(Cv)
     private readonly cvRepository: Repository<Cv>,
+    private readonly eventEmitter: EventEmitter2,
   ) {
     super(cvRepository);
   }
 
   async createForOwner(createCvDto: CreateCvDto, ownerId: number): Promise<Cv> {
+    this.eventEmitter.emit(
+      CvCreateStartedEvent.name,
+      new CvCreateStartedEvent(
+        ownerId,
+        createCvDto as unknown as Record<string, unknown>,
+      ),
+    );
+
     const cv = this.cvRepository.create({
       ...createCvDto,
       user: { id: ownerId },
       skills: createCvDto.skillIds?.map((id) => ({ id })) ?? [],
     });
 
-    return this.cvRepository.save(cv);
+    const savedCv = await this.cvRepository.save(cv);
+
+    this.eventEmitter.emit(
+      CvCreatedEvent.name,
+      new CvCreatedEvent(
+        ownerId,
+        savedCv.id,
+        savedCv as unknown as Record<string, unknown>,
+      ),
+    );
+
+    return savedCv;
   }
 
-  findAllForAdmin(): Promise<Cv[]> {
+  async findAllForAdmin(): Promise<Cv[]> {
     return this.cvRepository.find();
   }
 
-  findAllForUser(user: PayloadInterface): Promise<Cv[]> {
+  async findAllForUser(user: PayloadInterface): Promise<Cv[]> {
     if (user.role === Role.ADMIN) {
-      return this.findAllForAdmin();
+      const cvs = await this.findAllForAdmin();
+      this.emitReadEvents(cvs, user.sub);
+      return cvs;
     }
 
-    return this.cvRepository.find({ where: { user: { id: user.sub } } });
+    const cvs = await this.cvRepository.find({ where: { user: { id: user.sub } } });
+
+    this.emitReadEvents(cvs, user.sub);
+
+    return cvs;
   }
 
   async findAllForUserPaginated(
@@ -61,6 +93,8 @@ export class CvService extends GenericService<Cv> {
             take: normalizedLimit,
           });
 
+    this.emitReadEvents(data, user.sub);
+
     return {
       data,
       total,
@@ -71,6 +105,56 @@ export class CvService extends GenericService<Cv> {
   }
 
   async findOneForUser(id: number, user: PayloadInterface): Promise<Cv> {
+    const cv = await this.findOneAccessible(id, user);
+
+    this.eventEmitter.emit(CvReadEvent.name, new CvReadEvent(user.sub, cv.id));
+
+    return cv;
+  }
+
+  async updateForUser(
+    id: number,
+    updateCvDto: UpdateCvDto,
+    user: PayloadInterface,
+  ): Promise<Cv> {
+    this.eventEmitter.emit(
+      CvUpdateStartedEvent.name,
+      new CvUpdateStartedEvent(
+        user.sub,
+        id,
+        updateCvDto as unknown as Record<string, unknown>,
+      ),
+    );
+
+    const cv = await this.findOneAccessible(id, user);
+    const { skillIds, ...cvData } = updateCvDto;
+
+    Object.assign(cv, cvData);
+
+    if (skillIds !== undefined) {
+      cv.skills = skillIds.map((skillId) => ({ id: skillId }) as Skill);
+    }
+
+    const updatedCv = await this.cvRepository.save(cv);
+
+    this.eventEmitter.emit(
+      CvUpdatedEvent.name,
+      new CvUpdatedEvent(
+        user.sub,
+        updatedCv.id,
+        updatedCv as unknown as Record<string, unknown>,
+      ),
+    );
+
+    return updatedCv;
+  }
+
+  async removeForUser(id: number, user: PayloadInterface): Promise<void> {
+    const cv = await this.findOneAccessible(id, user);
+    await this.cvRepository.softDelete(cv.id);
+  }
+
+  private async findOneAccessible(id: number, user: PayloadInterface): Promise<Cv> {
     const cv =
       user.role === Role.ADMIN
         ? await this.cvRepository.findOne({ where: { id } })
@@ -85,25 +169,9 @@ export class CvService extends GenericService<Cv> {
     return cv;
   }
 
-  async updateForUser(
-    id: number,
-    updateCvDto: UpdateCvDto,
-    user: PayloadInterface,
-  ): Promise<Cv> {
-    const cv = await this.findOneForUser(id, user);
-    const { skillIds, ...cvData } = updateCvDto;
-
-    Object.assign(cv, cvData);
-
-    if (skillIds !== undefined) {
-      cv.skills = skillIds.map((skillId) => ({ id: skillId }) as Skill);
+  private emitReadEvents(cvs: Cv[], authorId: number): void {
+    for (const cv of cvs) {
+      this.eventEmitter.emit(CvReadEvent.name, new CvReadEvent(authorId, cv.id));
     }
-
-    return this.cvRepository.save(cv);
-  }
-
-  async removeForUser(id: number, user: PayloadInterface): Promise<void> {
-    const cv = await this.findOneForUser(id, user);
-    await this.cvRepository.softDelete(cv.id);
   }
 }
