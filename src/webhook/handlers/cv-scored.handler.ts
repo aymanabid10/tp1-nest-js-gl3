9bfbff0a90bb33be01aad73288512fa30a2160bc
Aymen Abid <aymen.abid2023@gmail.com>
2026-05-02 06:47:59 +0100

    fix : minor fixes

--- src/webhook/handlers/cv-scored.handler.ts
+++ src/webhook/handlers/cv-scored.handler.ts
@@ -5,12 +5,11 @@ import { CvService } from 'src/cv/cv.service';
 @Injectable()
 export class CvScoredHandler implements WebhookEventHandler {
   constructor(
-    private readonly cvService: CvService
   ) {}
   private readonly logger = new Logger(CvScoredHandler.name);
 
   async handle(data: any) {
     this.logger.log(`ATS CV scored for user ${data.userId}`);
-    return this.cvService.update(data.cvId, { score: data.score });
+    // TODO : update the CV score in the database
   }
 }
